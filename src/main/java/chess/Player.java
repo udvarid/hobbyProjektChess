e484c057f3c2ac3e4ba3963d28df85ceee557559
udvari.donat <udvari.donat@gmail.com>
2019-01-30 13:04:04 +0100

    the medium clever player is ready

--- src/main/java/chess/Player.java
+++ src/main/java/chess/Player.java
@@ -22,8 +22,8 @@ public class Player {
         //evaluates.add(new ValueBased(0));
         //evaluates.add(new ControlledCenters(0));
         //evaluates.add(new ControlledCells(0));
-        evaluates.add(new ValueBasedWithReattack(55));
-        evaluates.add(new CanGiveCheck(10));
+        evaluates.add(new ValueBasedWithReattack(60));
+        evaluates.add(new CanGiveCheck(5));
         evaluates.add(new ControlledCellsOnEnemySide(20));
         evaluates.add(new DevelopedLightOfficers(5));
         evaluates.add(new NumberOfSinglyPawns(5));
