90119fa89bbf3174fd09a0ce6c2aeb2b75f90871
udvari.donat <udvari.donat@gmail.com>
2019-01-31 13:17:31 +0100

    the medium clever player is ready

--- src/main/java/chess/Player.java
+++ src/main/java/chess/Player.java
@@ -11,6 +11,7 @@ public class Player {
     private Color enemyColor;
     private PlayerType type;
     private List<ValidMovePairWithScore> vms = new ArrayList<>();
+    private List<ValidMovePairWithScore> vmsEnemy = new ArrayList<>();
     private Random randomNumber = new Random();
     private List<Evaluate> evaluates = new ArrayList<>();
 
@@ -40,6 +41,162 @@ public class Player {
 
     public PlayerType getType() {
         return type;
+    }
+
+    public ValidMovePair giveCleverMove(Game game) {
+        ValidMovePair result = null;
+
+        for (ValidMovePair validMovePair : game.getValidmoves()) {
+            if (validMovePair.isMateTest() && validMovePair.getFigure().getColor() == this.color) {
+                return validMovePair;
+            }
+        }
+        Cloner cloner = new Cloner();
+
+        vms = new ArrayList<>();
+        int maxScore = Integer.MIN_VALUE;
+
+        for (ValidMovePair validMovePair : game.getValidmoves()) {
+            if (validMovePair.getFigure().getColor() == this.color) {
+
+                ValidMovePairWithScore vmsThis = new ValidMovePairWithScore(validMovePair);
+                vms.add(vmsThis);
+
+                Game gameCopy = cloner.deepClone(game);
+
+                gameCopy.deleteFigure(validMovePair.getEnd());
+
+                Figure figureToMove = null;
+                for (Figure figureSearched : gameCopy.getFigures()) {
+                    if (figureSearched.getActualPosition().equals(validMovePair.getStart())) {
+                        figureToMove = figureSearched;
+                        break;
+                    }
+                }
+
+                figureToMove.getActualPosition().setX(validMovePair.getEnd().getX());
+                figureToMove.getActualPosition().setY(validMovePair.getEnd().getY());
+
+                figureToMove.setStillInStartingPosition(false);
+
+                if (figureToMove.getFigureType() == FigureType.PAWN &&
+                        ((this.color == Color.BLACK && validMovePair.getEnd().getX() == 1) ||
+                                (this.color == Color.WHITE && validMovePair.getEnd().getX() == 8))) {
+                    gameCopy.promote(validMovePair, 'q');
+                }
+
+                gameCopy.finalValidMoves(true);
+                gameCopy.cleanFromChessRelatedMoves();
+
+                List<ValidMovePair> vpFromEnemy = movesFromEnemy(gameCopy);
+
+            }
+        }
+
+        //Végig megyünk az összes lehetséges lépéspáron DONE
+        //Ha tudunk mattot adni, megadjuk DONE
+        //Jelenlegi állást klónozzuk DONE
+        //Lelépjük DONE
+
+        //Átadjuk (minden lépést) az ellenfélnek DONE
+        //Ha az (az ellenfél) tud mattot adni, visszad egy 1 elemű listát, jelölve, hogy ez matt
+        //Amúgy klónozza az állást
+        //Lelép minden lépést, kiértékel
+        //A legjobbak közül 3-at (max) visszaad
+
+        //A kapott (max) 3 lépés alapján
+        //klónozunk
+        //lelépjük
+        //Ha tudunk mattot adni, akkor magas score-al (2000) számolunk
+        //Amúgy klónozzuk a táblát
+        //lelépjük a lépéseket és a kiértékelés alapján a max score-t visszaadjuk
+
+        //a (max) 3 score átlagát vesszük kiértékelés alapjául
+
+
+
+
+        return result;
+    }
+
+    private List<ValidMovePair> movesFromEnemy(Game gameCopy) {
+
+        List<ValidMovePair> listEnemyMoves = new ArrayList<>();
+
+        for (ValidMovePair validMovePair : gameCopy.getValidmoves()) {
+            if (validMovePair.isMateTest() && validMovePair.getFigure().getColor() == this.enemyColor) {
+                listEnemyMoves.add(validMovePair);
+                return listEnemyMoves;
+            }
+        }
+        Cloner clonerEnemy = new Cloner();
+
+        vmsEnemy = new ArrayList<>();
+        int maxScoreEnemy = Integer.MIN_VALUE;
+
+        for (ValidMovePair validMovePair : gameCopy.getValidmoves()) {
+            if (validMovePair.getFigure().getColor() == this.enemyColor) {
+
+                ValidMovePairWithScore vmsThis = new ValidMovePairWithScore(validMovePair);
+                vmsEnemy.add(vmsThis);
+
+                Game gameCopyEnemy = clonerEnemy.deepClone(gameCopy);
+
+                gameCopyEnemy.deleteFigure(validMovePair.getEnd());
+
+                Figure figureToMove = null;
+                for (Figure figureSearched : gameCopyEnemy.getFigures()) {
+                    if (figureSearched.getActualPosition().equals(validMovePair.getStart())) {
+                        figureToMove = figureSearched;
+                        break;
+                    }
+                }
+
+                figureToMove.getActualPosition().setX(validMovePair.getEnd().getX());
+                figureToMove.getActualPosition().setY(validMovePair.getEnd().getY());
+
+                figureToMove.setStillInStartingPosition(false);
+
+                if (figureToMove.getFigureType() == FigureType.PAWN &&
+                        ((this.enemyColor == Color.BLACK && validMovePair.getEnd().getX() == 1) ||
+                                (this.enemyColor == Color.WHITE && validMovePair.getEnd().getX() == 8))) {
+                    gameCopyEnemy.promote(validMovePair, 'q');
+                }
+
+                gameCopyEnemy.finalValidMoves(true);
+                gameCopyEnemy.cleanFromChessRelatedMoves();
+
+                int scoreToSet = 0;
+                for (Evaluate evaluate : this.evaluates) {
+                    int partScore = evaluate.score(this.enemyColor, this.color, gameCopy) * evaluate.getWeight() / 100;
+                    scoreToSet += partScore;
+                }
+                vmsThis.setScore(scoreToSet);
+
+                if (maxScoreEnemy < scoreToSet) {
+                    maxScoreEnemy = scoreToSet;
+                }
+
+
+            }
+        }
+
+        Collections.sort(vmsEnemy);
+        Iterator<ValidMovePairWithScore> iterator = vmsEnemy.iterator();
+        while (iterator.hasNext()) {
+            ValidMovePairWithScore vmsToCheck = iterator.next();
+            if (vmsToCheck.getScore() < maxScoreEnemy) {
+                iterator.remove();
+            }
+        }
+
+        for (int i = 0; i < Math.min(vmsEnemy.size(), 3); i++) {
+            listEnemyMoves.add(vmsEnemy.get(i).getValidMovePair());
+        }
+
+        return listEnemyMoves;
+
+
     }
 
     public ValidMovePair giveMove(Game game) {
